457cad2f33a8dbe8532928c433395ee47933a89f
rajprabhuin@gmail.com <Aaditya8#>
2019-02-02 17:08:41 +0530

    Feb First Learn

---
+++ GOLEARNFEB/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Rajkumar. You need to Learn to Code in GO")
+	for i := 1; i <= 20; i++ {
+		fmt.Println(i)
+	}
+
+	foo()
+
+}
+
+func foo() {
+	fmt.Println("Hello This is Rajkumar. See How i fmt my code")
+}
