1d05f73df1111d89612481fa4055a02952eee83e
rajprabhuin@gmail.com <Aaditya8#>
2019-02-03 09:41:58 +0530

    Print Even Numbers

--- GOLEARNFEB/main.go
+++ GOLEARNFEB/main.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("Hello Rajkumar. You need to Learn to Code in GO")
 	for i := 1; i <= 20; i++ {
-		fmt.Println(i)
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
 	}
 
 	foo()
